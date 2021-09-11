f0a2b2e282895f9b70a7f836c4fa48c0306ba686
Salathiel Vieira <salathielvieira@outlook.com>
2021-09-11 00:47:51 -0400

    Pagination

---
+++ backend/src/main/java/com/javaspringboot/vendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.javaspringboot.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javaspringboot.vendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
